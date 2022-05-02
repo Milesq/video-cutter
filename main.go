ad53a35520508d72ae084930b143edfaf09029da
Milesq <wmilosz88@gmail.com>
2022-05-02 11:38:08 +0200

    feat: add config template

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
+	"os"
+)
 
 const configPath = "config.json"
 
+//go:embed configTemplate.json
+var configTemplate []byte
+
 func main() {
+	if !fileExists(configPath) {
+		createDefaultConfig()
+		return
+	}
+
 	fmt.Println("cut...")
 	config := getConfigFromFile()
-
 	cutter(config)
+}
+
+func createDefaultConfig() {
+	os.WriteFile(configPath, configTemplate, 0666)
 }
